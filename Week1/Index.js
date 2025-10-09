7a2668fa5d60ddc3200758b92b2822e0832d10fb
Trilochan777 <sanatpandey464@gmail.com>
2025-10-09 16:44:13 +0000

    week 1 50006

---
+++ Week1/Index.js
@@ -0,0 +1,8 @@
+console.log("This is my first program")
+console.log("well come trilochan your salary is 500000")
+const num1=5;
+const num2=3;
+//add two number num1 and num2;
+const sum = num1 + num2;
+//display the sumcd
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
